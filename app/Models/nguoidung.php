c9bccd1892c5d02b2b784adcb796d751f80f557f
thanh2107 <tyn01685732770@gmail.com>
2020-09-15 22:36:55 +0700

    Tạo csdl, kết nối db , tạo model , tạo quan hệ

---
+++ app/Models/nguoidung.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+protected $table = 'nguoi_dung'
+class nguoidung extends Model
+{
+    use HasFactory;
+}
